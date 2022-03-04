4e5e1a2e903897e6155af736e68e679294950236
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-03-04 23:54:05 +0000

    variables and getters

--- craftingWithTrees/src/Slot.java
+++ craftingWithTrees/src/Slot.java
@@ -1,3 +1,15 @@
 public class Slot
 {
+    private int amt;
+    private Item item;
+
+    public int getAmt()
+    {
+        return amt;
+    }
+
+    public Item getItem()
+    {
+        return item;
+    }
 }
