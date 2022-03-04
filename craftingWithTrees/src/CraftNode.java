507027502998e0061b478e86050643fe0efafb1f
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-03-04 23:51:55 +0000

    variables and part of constructor

--- craftingWithTrees/src/CraftNode.java
+++ craftingWithTrees/src/CraftNode.java
@@ -1,3 +1,12 @@
 public class CraftNode
 {
+    private Slot toCraft;
+    private CraftNode[] childNodes;
+
+    public CraftNode(Slot toCraft)
+    {
+        this.toCraft = toCraft;
+        this.childNodes = new CraftNode[toCraft.getItem().getRecipe().getRecipeArr().length];
+        
+    }
 }
