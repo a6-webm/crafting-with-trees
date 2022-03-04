36bcc745ea6ca2749b667a8cba82f0aa162d8bf0
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-03-04 23:58:49 +0000

    variables and getters

--- craftingWithTrees/src/Recipe.java
+++ craftingWithTrees/src/Recipe.java
@@ -1,3 +1,15 @@
 public class Recipe
 {
+    private int yield;
+    private Slot[] recipeArr;
+
+    public int getYield()
+    {
+        return yield;
+    }
+
+    public Slot[] getRecipeArr()
+    {
+        return recipeArr;
+    }
 }
