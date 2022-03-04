43ab82ffa9cb499c2f6f825f88794fd7cb4ddd52
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-03-04 23:43:39 +0000

    getter and constructor

--- craftingWithTrees/src/CraftTree.java
+++ craftingWithTrees/src/CraftTree.java
@@ -1,3 +1,14 @@
 public class CraftTree
 {
+    private CraftNode root;
+
+    public CraftNode getRoot()
+    {
+        return root;
+    }
+
+    public CraftTree(Slot root)
+    {
+        this.root = new CraftNode(root);
+    }
 }
