89209321821caba1c4ebd7d4c4f9d4e5d3942d2a
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-03-05 01:22:19 +0000

    finished constructor; now inits childNodes

--- craftingWithTrees/src/CraftNode.java
+++ craftingWithTrees/src/CraftNode.java
@@ -6,10 +6,12 @@ public class CraftNode
     public CraftNode(Slot toCraft)
     {
         this.toCraft = toCraft;
-        this.childNodes = new CraftNode[toCraft.getItem().getRecipe().getRecipeArr().length];
-        for (Slot recipeSlot : toCraft.getItem().getRecipe().getRecipeArr())
-        {
+        this.childNodes = new CraftNode[Item.getItem(toCraft.getItemID()).getRecipe().getRecipeArr().length];
 
+        for (int i = 0; i < childNodes.length; i++) // TODO this might be a little hasty, possibly leave this functionality in CraftTree
+        {
+            Slot st = Item.getItem(toCraft.getItemID()).getRecipe().getRecipeArr()[i];
+            childNodes[i] = new CraftNode(st);
         }
     }
 }
