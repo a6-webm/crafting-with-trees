6120bb03f68ccb950aaf49a820ed54716f5d9fba
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-03-05 00:20:36 +0000

    not using enum anymore

--- craftingWithTrees/src/Item.java
+++ craftingWithTrees/src/Item.java
@@ -1,3 +1,28 @@
-public enum Item
+public class Item
 {
+    
+    private String id;
+    private String name;
+    private boolean doCraft;
+    private Recipe recipe;
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public boolean isDoCraft()
+    {
+        return doCraft;
+    }
+
+    public Recipe getRecipe()
+    {
+        return recipe;
+    }
 }
