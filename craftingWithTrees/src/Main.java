252ede028ba2049c5fdde60a5d26c5f45f05f2f2
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-03-05 01:23:04 +0000

    added wooden plank test item

--- craftingWithTrees/src/Main.java
+++ craftingWithTrees/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        
+        new Item("wooden_plank",
+                "Wooden Plank",
+                true,
+                new Recipe(4, new Slot[]{new Slot(1, "oak_log")})
+        );
+
     }
 }
