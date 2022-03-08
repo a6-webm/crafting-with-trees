f843dc25e3ef0d78b341cbc71ef7d7fb5ca2ebb3
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-03-08 22:36:36 +0000

    added variables and constructors

--- craftWTreesProj/src/main/java/main/Craft.java
+++ craftWTreesProj/src/main/java/main/Craft.java
@@ -2,5 +2,18 @@ package main;
 
 public class Craft
 {
+    public int numOfCrafts;
+    public int numRequired;
 
+    public Craft()
+    {
+        this.numOfCrafts = 0;
+        this.numRequired = 0;
+    }
+
+    public Craft(int numOfCrafts, int numRequired)
+    {
+        this.numOfCrafts = numOfCrafts;
+        this.numRequired = numRequired;
+    }
 }
