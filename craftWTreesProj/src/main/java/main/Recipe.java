568ac4089a718f7bfa74495da6ea3df7d11f7147
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-03-08 23:28:10 +0000

    added functionality for not consuming certain ingredients

--- craftWTreesProj/src/main/java/main/Recipe.java
+++ craftWTreesProj/src/main/java/main/Recipe.java
@@ -4,6 +4,7 @@ public class Recipe
 {
     private int yield;
     private Slot[] recipeArr;
+    private boolean[] consumesCraft;
 
     public int getYield()
     {
@@ -15,15 +16,31 @@ public class Recipe
         return recipeArr;
     }
 
+    public boolean[] getConsumesCraft()
+    {
+        return consumesCraft;
+    }
+
     public Recipe()
     {
         this.yield = 0;
         this.recipeArr = new Slot[0];
+        this.consumesCraft = new boolean[0];
     }
 
     public Recipe(int yield, Slot[] recipeArr)
     {
         this.yield = yield;
         this.recipeArr = recipeArr;
+        this.consumesCraft = new boolean[recipeArr.length];
+        for (int i = 0; i < this.consumesCraft.length; i++)
+            this.consumesCraft[i] = true;
+    }
+
+    public Recipe(int yield, Slot[] recipeArr, boolean[] consumesCraft)
+    {
+        this.yield = yield;
+        this.recipeArr = recipeArr;
+        this.consumesCraft = consumesCraft;
     }
 }
