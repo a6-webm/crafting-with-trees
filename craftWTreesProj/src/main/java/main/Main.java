a924d265d4d349aceefbc43d227f9028c2cbfed2
a6.webm <46289515+a6-webm@users.noreply.github.com>
2022-03-08 22:51:05 +0000

    added recipe for wooden axe

--- craftWTreesProj/src/main/java/main/Main.java
+++ craftWTreesProj/src/main/java/main/Main.java
@@ -10,13 +10,26 @@ public class Main
 
     public static void main(String[] args)
     {
-
+        new Item("wooden_plank",
+                "Wooden Plank",
+                true,
+                new Recipe(4, new Slot[]{new Slot(1, "oak_log")})
+        );
+        new Item("stick",
+                "Stick",
+                true,
+                new Recipe(4, new Slot[]{new Slot(2, "wooden_plank")})
+        );
+        new Item("wooden_axe",
+                "Wooden Axe",
+                true,
+                new Recipe(1, new Slot[]{new Slot(3, "wooden_plank"), new Slot(2,"stick")})
+        );
     }
 
     static String makeCraftItinerary(Slot itemToBeCrafted)
     {
         HashMap<String,Craft> totalCrafts = new HashMap<String,Craft>();
-
         MutableValueGraph<String,Boolean> craftGraph = ValueGraphBuilder.directed().build();
 
         return "";
